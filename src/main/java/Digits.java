c7208f4d726e3d91e00e8968a18eae3a0cf4f687
kevlao1 <kevinlao2k20@gmail.com>
2025-04-11 23:30:36 -0700

    Update Digits.java

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -9,16 +9,9 @@ public class Digits
 	{ 
 		digitList = new ArrayList<Integer>();
 		int origNum  = num;
-		String uhOh = Integer.toString(num);
-		int checker = uhOh.length();
 		while(origNum > 0){
 			digitList.add(0, origNum%10);
 			origNum = origNum/10;
-		}
-	while(checker > digitList.size()){
-		if(checker > digitList.size()){
-				digitList.add(0, 0);
-			}
 		}
 	}
 
